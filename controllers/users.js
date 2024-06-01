8a7915d1b259ca64dbbe25d3aef2172ccaeba035
Katherine Kelly <ktkelly11@gmail.com>
2024-06-01 14:55:02 -0400

    Updated users

--- controllers/users.js
+++ controllers/users.js
@@ -6,9 +6,6 @@ module.exports = {
   getUserById,
   updateUser,
   deleteUser,
-  findByEmail,
-  addSkills,
-  findUsersOver21,
 };
 
 async function createUser(req, res) {
@@ -29,16 +26,6 @@ async function createUser(req, res) {
 async function getUsers(req, res) {
   try {
     const users = await User.find({});
-
-    res.status(200).json(users);
-  } catch (err) {
-    res.status(400).send(err);
-  }
-}
-
-async function findUsersOver21(req, res) {
-  try {
-    const users = await User.findOver21();
 
     res.status(200).json(users);
   } catch (err) {
@@ -72,25 +59,6 @@ async function updateUser(req, res) {
   }
 }
 
-// Add a skill to the skills array
-async function addSkills(req, res) {
-  try {
-    // The skills we will add will be in the req.body
-    // Using the model to find the user by Id
-    const foundUser = await User.findById(req.params.id);
-
-    const concatedArray = foundUser.skills.concat(req.body.skills);
-
-    foundUser.skills = concatedArray;
-
-    await foundUser.save();
-
-    res.send(foundUser);
-  } catch (err) {
-    res.status(400).send(err);
-  }
-}
-
 // Delte a single user by ID
 async function deleteUser(req, res) {
   try {
@@ -99,16 +67,6 @@ async function deleteUser(req, res) {
     res.status(200).json({
       message: "Successfully Deleted the User",
     });
-  } catch (err) {
-    res.status(400).send(err);
-  }
-}
-
-async function findByEmail(req, res) {
-  try {
-    const foundUser = await User.findOne({ email: req.params.email });
-
-    res.status(200).json(foundUser);
   } catch (err) {
     res.status(400).send(err);
   }
--- routes/grades.js
+++ routes/grades.js
@@ -3,6 +3,8 @@ const gradesCtrl = require("../controllers/grades.js");
 
 const router = Router();
 
+router.post("/", gradesCtrl.createGrade);
+
 router.get("/", gradesCtrl.getGrades);
 
 module.exports = router;
