ee7e112d0c33a2050a3e2d041a3dbdbe21a737e8
Katherine Kelly <ktkelly11@gmail.com>
2024-06-01 14:00:56 -0400

    Added grades route

--- models/User.js
+++ models/User.js
@@ -33,18 +33,4 @@ const userSchema = Schema({
   whatever: String,
 });
 
-// Arrow functions do not support the "this keyword"
-
-// Instance Schema method
-userSchema.methods.logUserInfo = function () {
-  console.log(
-    `I am ${this.age} years old and I am ${this.isCool ? "" : "not"} cool`
-  );
-};
-
-// Static Schema methods
-userSchema.statics.findOver21 = function () {
-  this.find({ age: { $gte: 21 } });
-};
-
 module.exports = model("User", userSchema);
---
+++ routes/users.js
@@ -0,0 +1,22 @@
+const { Router } = require("express");
+const usersCtrl = require("../controllers/users.js");
+
+const router = Router();
+
+router.post("/", usersCtrl.createUser);
+
+router.get("/", usersCtrl.getUsers);
+
+router.get("/over21", usersCtrl.findUsersOver21);
+
+router.get("/email/:email", usersCtrl.findByEmail);
+
+router.get("/:id", usersCtrl.getUserById);
+
+router.put("/:id", usersCtrl.updateUser);
+
+router.patch("/:id/skills/add", usersCtrl.addSkills);
+
+router.delete("/:id", usersCtrl.deleteUser);
+
+module.exports = router;
